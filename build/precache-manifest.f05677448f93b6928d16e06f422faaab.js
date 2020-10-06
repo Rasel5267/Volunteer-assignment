0d7dfbb8a4b8f1972fbc64a47977ba0df9dd6da0
Rasel5267 <mahadiul09@gmail.com>
2020-10-06 13:26:48 +0600

    build folder create

---
+++ build/precache-manifest.f05677448f93b6928d16e06f422faaab.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36cac9221b17910f8a682362695be3cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d21efa2f3c59a1f568a5",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "420d50d33740b4e59367",
+    "url": "/static/css/main.3223bda9.chunk.css"
+  },
+  {
+    "revision": "d21efa2f3c59a1f568a5",
+    "url": "/static/js/2.050c25d9.chunk.js"
+  },
+  {
+    "revision": "86693a4c92dc2835d480d760434fd4c0",
+    "url": "/static/js/2.050c25d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "420d50d33740b4e59367",
+    "url": "/static/js/main.fe75473f.chunk.js"
+  },
+  {
+    "revision": "5e9ce65f8f068e7d1fcd",
+    "url": "/static/js/runtime-main.3ce41d22.js"
+  },
+  {
+    "revision": "b7dd8bbef4a0ef3ceba26c40007aaf82",
+    "url": "/static/media/Group 1329.b7dd8bbe.png"
+  },
+  {
+    "revision": "01131059dee70422b520086d7e109d1c",
+    "url": "/static/media/extraVolunteer.01131059.png"
+  }
+]);
